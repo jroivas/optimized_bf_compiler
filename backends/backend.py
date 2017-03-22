a4751e400c191843eb0e535a397c4fe10f6e0863
Jouni Roivas <jroivas@iki.fi>
2017-03-22 20:14:05 +0200

    Add missing backend file

---
+++ backends/backend.py
@@ -0,0 +1,17 @@
+class Backend:
+    def __init__(name, flags):
+        self.name = name
+        self.flags = flags
+
+    def translate(self, data, blocks):
+        pass
+
+    def compile(self):
+        pass
+
+    def write(self):
+        pass
+
+    def binaryName(self):
+        return self.binary_name
+
